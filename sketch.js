bde4ab35e50dad3f45e6a71f1cd813818fbef7c7
megan schulte <megan.schulte.pdx@gmail.com>
2020-02-17 13:49:00 -0800

    add rotation

--- sketch.js
+++ sketch.js
@@ -16,7 +16,9 @@ const settings = {
 
 // capture artwork in single function, returns render function
 const sketch = () => {
-	const palette = random.pick(palettes);
+	// make palette random number of colors each reload
+	const colorCount = random.rangeFloor(2, 6);
+	const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
 	console.log(palette);
 	// create local state
 	const createGrid = () => {
@@ -26,12 +28,16 @@ const sketch = () => {
 			for (let y = 0; y < count; y++) {
 				//divides grid between 0 and 1
 				const u = count <= 1 ? 0.5 : x / (count - 1);
-				const v = count < +1 ? 0.5 : y / (count - 1);
+				const v = count <= 1 ? 0.5 : y / (count - 1);
+				const radius = Math.abs(random.noise2D(u, v)) * 0.2;
 				//give circles different radius
+				// object
 				points.push({
 					color: random.pick(palette),
+					radius,
+					rotation: random.noise2D(u, v) * 1,
 					//0.01 give start value for circles then adding to that
-					radius: Math.abs(0.01 + random.value() * 0.01),
+					// radius: Math.abs(0.01 + random.gaussian() * 0.01),
 					position: [u, v]
 				});
 			}
@@ -40,33 +46,40 @@ const sketch = () => {
 	};
 
 	// sets deterministic seed so that the random number string stays the same and the image doesn't change on reload
-	random.setSeed(512);
-	const points = createGrid().filter(() => random.gaussian() > 0.01);
-
-	const margin = 100;
+	// random.setSeed(512);
+	const points = createGrid().filter(() => random.value() > 0.5);
+	const margin = 400;
 
 	return ({ context, width, height }) => {
 		// const { position, radius } = data;
 
-		context.fillStyle = "pink";
+		context.fillStyle = "white";
 		context.fillRect(0, 0, width, height);
 
 		// destructuring data
 		points.forEach(data => {
-			const { position, radius, color } = data;
+			const { position, radius, color, rotation } = data;
 
 			//destructure value for u and v
 			const [u, v] = position;
-
 			const x = lerp(margin, width - margin, u);
 			const y = lerp(margin, height - margin, v);
 
-			//draw a circle
-			context.beginPath();
-			context.arc(x, y, radius * width, Math.PI * 2, false);
-			context.fillStyle = "red";
-			context.lineWidth = "10";
-			context.fill();
+			// //draw a circle
+			// context.beginPath();
+			// context.arc(x, y, radius * width, Math.PI * 2, false);
+			// context.fillStyle = color;
+			// context.lineWidth = "20";
+			// context.fill();
+
+			context.save();
+			context.fillStyle = color;
+			context.font = `${radius * width}px "Helvetica"`;
+			context.translate(x, y);
+			context.rotate(rotation);
+			context.fillText("=", 0, 0);
+
+			context.restore();
 		});
 	};
 };
