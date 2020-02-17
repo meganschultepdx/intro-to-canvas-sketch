e73d101d730bf458b25da854ada9883c95e6b648
megan schulte <megan.schulte.pdx@gmail.com>
2020-02-17 11:40:53 -0800

    add canvas-sketch-util and lerp from it

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 // requiring javascript library using Nodejs require syntax
 const canvasSketch = require("canvas-sketch");
+//lerp is a func for linear interpalation between min and max using the t parameter.
+const { lerp } = require("canvas-sketch-util/math");
 
 // define parameters for artwork (size, export settings, etc.)
 const settings = {
@@ -11,20 +13,40 @@ const settings = {
 
 // capture artwork in single function, returns render function
 const sketch = () => {
+	// create local state
+	const createGrid = () => {
+		const points = [];
+		const count = 5;
+		for (let x = 0; x < count; x++) {
+			for (let y = 0; y < count; y++) {
+				//divides grid between 0 and 1
+				const u = count <= 1 ? 0.5 : x / (count - 1);
+				const v = count < +1 ? 0.5 : y / (count - 1);
+				points.push([u, v]);
+			}
+		}
+		return points;
+	};
+
+	const points = createGrid();
+
+	const margin = 400;
+
 	return ({ context, width, height }) => {
-		console.log(width, height);
-		context.fillStyle = "orange";
+		context.fillStyle = "white";
 		context.fillRect(0, 0, width, height);
 
-		//begin by rendering path, then everything after will be part of that path operation
-		context.beginPath();
-		context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-		context.fillStyle = "red";
-		context.fill();
-		//take whatever path you have before it and outline in a black stroke
-		context.lineWidth = width * 0.05;
-		// context.strokeStyle = "yellow";
-		context.stroke();
+		points.forEach(([u, v]) => {
+			const x = lerp(margin, width - margin, u);
+			const y = lerp(margin, height - margin, v);
+
+			//draw a circle
+			context.beginPath();
+			context.arc(x, y, 150, Math.PI * 2, false);
+			context.strokeStyle = "black";
+			context.lineWidth = "25";
+			context.stroke();
+		});
 	};
 };
 
