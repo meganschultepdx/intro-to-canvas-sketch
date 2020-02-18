4b83c7aab6bde9569365c985853f747ff323587a
megan schulte <megan.schulte.pdx@gmail.com>
2020-02-17 19:28:58 -0800

    instal glsl noise

--- shader.js
+++ shader.js
@@ -1,11 +1,12 @@
-const canvasSketch = require('canvas-sketch');
-const createShader = require('canvas-sketch-util/shader');
-const glsl = require('glslify');
+const canvasSketch = require("canvas-sketch");
+const createShader = require("canvas-sketch-util/shader");
+// module that we use to write out shader bc not javascript, can import utilities
+const glsl = require("glslify");
 
 // Setup our sketch
 const settings = {
-  context: 'webgl',
-  animate: true
+	context: "webgl",
+	animate: true
 };
 
 // Your glsl code
@@ -13,28 +14,46 @@ const frag = glsl(`
   precision highp float;
 
   uniform float time;
+  uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
+    // vec3 colorA = sin(time) + vec3(1.0, 0.0, 0.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
+
+    // vec2 center = vUv - 0.5;
+    // center.x *= aspect;
+    // float dist = length(center);
+
+    // float alpha = smoothstep(0.251, 0.25, dist);
+
+    // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+    // gl_FragColor = vec4(color, alpha);
+
+    float n = noise(vec3(vUv.xy, time));
+
+    gl_FragColor = vec4(vec3(vUv.x), 1.0);
   }
 `);
 
 // Your sketch, which simply returns the shader
 const sketch = ({ gl }) => {
-  // Create the shader and return it
-  return createShader({
-    // Pass along WebGL context
-    gl,
-    // Specify fragment and/or vertex shader strings
-    frag,
-    // Specify additional uniforms to pass down to the shaders
-    uniforms: {
-      // Expose props from canvas-sketch
-      time: ({ time }) => time
-    }
-  });
+	// Create the shader and return it
+	return createShader({
+		clearColor: "white",
+		// Pass along WebGL context
+		gl,
+		// Specify fragment and/or vertex shader strings
+		frag,
+		// Specify additional uniforms to pass down to the shaders
+		uniforms: {
+			// Expose props from canvas-sketch
+			time: ({ time }) => time,
+			aspect: ({ width, height }) => width / height
+		}
+	});
 };
 
 canvasSketch(sketch, settings);
