5faafca553de4adfba8541a0501c081e5656ecb8
megan schulte <megan.schulte.pdx@gmail.com>
2020-02-17 20:14:50 -0800

    install glsl-hsl2rgb

--- shader.js
+++ shader.js
@@ -6,7 +6,10 @@ const glsl = require("glslify");
 // Setup our sketch
 const settings = {
 	context: "webgl",
-	animate: true
+	dimensions: [512, 512],
+	animate: true,
+	duration: 4,
+	fps: 24
 };
 
 // Your glsl code
@@ -18,23 +21,31 @@ const frag = glsl(`
   varying vec2 vUv;
 
   #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
   void main () {
     // vec3 colorA = sin(time) + vec3(1.0, 0.0, 0.0);
     // vec3 colorB = vec3(0.0, 1.0, 0.0);
 
-    // vec2 center = vUv - 0.5;
-    // center.x *= aspect;
-    // float dist = length(center);
+    vec2 center = vUv - 0.5;
+    center.x *= aspect;
 
-    // float alpha = smoothstep(0.251, 0.25, dist);
+    float dist = length(center);
+
+    float alpha = smoothstep(0.251, 0.25, dist);
 
     // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
     // gl_FragColor = vec4(color, alpha);
 
-    float n = noise(vec3(vUv.xy, time));
+    float n = noise(vec3(center * 1.0, time * 0.55));
 
-    gl_FragColor = vec4(vec3(vUv.x), 1.0);
+    vec3 color = hsl2rgb(
+        0.6 + n * 0.1, 
+        0.5,
+        0.5
+    );
+
+    gl_FragColor = vec4(color, alpha);
   }
 `);
 
